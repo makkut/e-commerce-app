c0a177a0851cd483d80ba149dbff9cad96e24dfc
Maxim Kutprot <maksim.kutprot@daubit.org>
2023-10-18 22:36:16 +0200

    overview

--- components/settings-form.tsx
+++ components/settings-form.tsx
@@ -22,6 +22,7 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { AlertModal } from "./modals/alert-modal";
+import { ApiAlert } from "./ui/api-alert";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -122,6 +123,12 @@ export const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
           </Button>
         </form>
       </Form>
+      <Separator />
+      <ApiAlert
+        title="NEXT_PUBLIC_API_URL"
+        description={`${origin}/api/${params.storeId}`}
+        variant="public"
+      />
     </>
   );
 };
